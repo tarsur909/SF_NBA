9a5f62b6d3ad63c8f3523bf8c15d0e2de18c67a7
tarsur909 <68882529+tarsur909@users.noreply.github.com>
2021-08-16 15:27:47 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Tarun Suresh',                   # Type in your name
   author_email = 'tarsur909@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/tarsur909/SFNBA',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/tarsur909/SFNBA/archive/refs/tags/v_01.tar.gz',    # I explain this later on 
+  download_url = 'https://github.com/tarsur909/SFNBA/archive/refs/tags/v_02.tar.gz',    # I explain this later on 
   keywords = ['NBA', 'Machine Learning', 'Time Series Forecasting'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'matplotlib',
