b2ebd1628139dbfb271cf29bcc478c67ae8f2ebe
tarsur909 <68882529+tarsur909@users.noreply.github.com>
2021-08-17 09:53:23 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'sf_nba',         # How you named your package folder (MyLib)
   packages = ['sf_nba'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Simulate NBA Games Between Custom Teams and Forecast Player Stats in Future Games',   # Give a short description about your library
   author = 'Tarun Suresh',                   # Type in your name
