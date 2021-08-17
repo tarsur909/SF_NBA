451b9081cd5782c9a25cc3df663b3ea9b8ecb451
tarsur909 <68882529+tarsur909@users.noreply.github.com>
2021-08-17 09:11:11 -0700

    Update NextXGamesForecast.py

--- sfnba/NextXGamesForecast.py
+++ sfnba/NextXGamesForecast.py
@@ -2,7 +2,7 @@ class NextXGamesForecast():
   def __init__(self, player_name, stat = 'PTS'):
     import pandas as pd
     import numpy as np
-    df  = pd.read_csv('https://media.githubusercontent.com/media/tarsur909/SFNBA/main/sfnba/new_player_tables11.csv?token=AQNRAYPY76FHNK35GZYQKOTBDLZLM')
+    df  = pd.read_csv('https://media.githubusercontent.com/media/tarsur909/SFNBA/main/data/final_player_tables.csv?token=AQNRAYNUIPWES7O74DXDRK3BDPWCQ')
     self.df = df
 
     #store input data
